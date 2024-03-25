3d689f5f41773a53fe77f80339b9387ca2c0cd66
Elias Jurado <ridimdev@gmail.com>
2024-03-25 18:41:43 -0500

    levantando servidor

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "hola mundo")
+	})
+
+	port := ":8080"
+	fmt.Printf("Servidor escuchando en http://localhost%s", port)
+	http.ListenAndServe(port, nil)
 }
