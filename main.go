0fc4d5beeba180fb7893e0c49e38779816125d8d
Elias Jurado <ridimdev@gmail.com>
2024-03-25 18:49:39 -0500

    crear rutas para handlers

--- main.go
+++ main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"rpsweb/handlers"
 )
 
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("/",Index)
+	router.HandleFunc("/", handlers.Index)
+	router.HandleFunc("/new", handlers.NewGame)
+	router.HandleFunc("/game", handlers.Game)
+	router.HandleFunc("/play", handlers.Play)
+	router.HandleFunc("/about", handlers.About)
 
 	port := ":8080"
 	fmt.Printf("Servidor escuchando en http://localhost%s", port)
 	http.ListenAndServe(port, router)
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hola mundo")
 }
